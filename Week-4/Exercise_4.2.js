51ed932f92e864693726b0ef891c728503b7d17f
nsummit <nsummit10@gmail.com>
2023-02-17 02:29:05 +0530

    Week 4 Assignment Completed

---
+++ Week-4/Exercise_4.2.js
@@ -0,0 +1,15 @@
+var Person = function() {};
+Person.prototype.initialize = function(name, age) {
+  this.name = name;
+  this.age = age;
+};
+
+var Teacher = function() {};
+Teacher.prototype = Object.create(Person.prototype);
+Teacher.prototype.teach = function(subject) {
+  console.log(`${this.name} is now teaching ${subject}`);
+};
+
+var him = new Teacher();
+him.initialize("Shonn", 45);
+him.teach("Inheritance");
